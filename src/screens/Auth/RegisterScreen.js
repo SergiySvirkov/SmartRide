2d76ab27f10517f12240aab07c4c47448c1b474f
SergiySvirkov <124861145+SergiySvirkov@users.noreply.github.com>
2023-12-14 15:35:39 +0200

    Add files via upload

---
+++ src/screens/Auth/RegisterScreen.js
@@ -0,0 +1,76 @@
+// RegisterScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
+import authService from '../../services/authService';
+
+const RegisterScreen = ({ navigation }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleRegister = async () => {
+    try {
+      // Perform registration logic using authService
+      const credentials = { username, password };
+      await authService.register(credentials);
+      // Navigate to the main app screen upon successful registration
+      navigation.navigate('Main');
+    } catch (error) {
+      console.error('Registration failed:', error);
+      // Handle registration error, e.g., show an error message
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Register</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Username"
+        onChangeText={(text) => setUsername(text)}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry
+        onChangeText={(text) => setPassword(text)}
+      />
+      <TouchableOpacity style={styles.button} onPress={handleRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  input: {
+    width: '80%',
+    padding: 10,
+    marginBottom: 20,
+    borderWidth: 1,
+    borderRadius: 5,
+  },
+  button: {
+    backgroundColor: '#3498db',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default RegisterScreen;
+
